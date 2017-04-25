332767975bcd9f4f87a927f44b958359d2eccd90
tmp <tmp@phone-wifi.local>
2017-04-25 11:23:22 +0800

    update

--- auto-update.sh
+++ auto-update.sh
@@ -5,6 +5,7 @@ git add .
 git commit -m "v${ver}"
 git tag -a ${ver} -m "v${ver}"
 git remote add origin git@github.com:rcalc/sdk-ios.git
+git remote set-url origin git@github.com:rcalc/sdk-ios.git
 git push -u origin master --tags
 
 pod trunk push rcsdk.podspec
